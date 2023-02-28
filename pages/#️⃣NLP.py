eac33dbdec12df64e2b0c992418518065fb91454
AlekHesa <91420580+AlekHesa@users.noreply.github.com>
2023-02-28 10:53:29 +0700

    Ganti jadi pakai Sidebar

---
+++ pages/#️⃣NLP.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from GPT3 import *
+from streamlit_chat import message
+import time
+
+
+
+tab1,tab2 = st.tabs(["Summarizer","Generate Text"])
+
+    
+with tab1:
+    st.header("Summarization")
+
+    input_summary = st.text_area("Put your text")
+
+    summary = "Summarize this text :"
+    sum = list()
+    sum.append(input_summary)
+    if input_summary: 
+        test = summary + input_summary
+        res = proccess(test)
+        st.write(res)
+    else:
+        st.write("Please enter a text")   
+with tab2 :
+    st.header("Generate the text")
+    
+    input_gpt3 = st.text_input("What do you want to make?")
+    
+    if input_gpt3:
+        res = proccess(input_gpt3)
+        st.write(res)
+    else:
+        st.write("Please enter a text")
---
+++ pages/🤖Chatbot.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from GPT3 import *
+from streamlit_chat import message
+import time
+
+st.set_page_config(
+    page_title="Chatbot",
+    page_icon="🤖"
+)
+
+st.header("GPT-3 Chatbot with Characteristics")
+def text():
+    input_user = st.text_input("USER:",key="input")
+    
+    
+    return input_user
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+characteristics = st.text_input("Characteristics",key="char")
+
+if characteristics:
+    user_input = text()
+    convo = list()
+    convo.append('USER: %s' % user_input)
+    text_block = '\n'.join(convo)
+    char = characteristics + text_block+"\nJAX: "
+    res = chatbot(char)
+
+
+    if user_input:
+        output = chatbot(user_input)
+
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(res)
+
+    if st.session_state['generated']:
+
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
