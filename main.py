02f09d7b779b83356a1f182067eb912cc0e1d9cf
AlekHesa <91420580+AlekHesa@users.noreply.github.com>
2023-02-28 10:54:36 +0700

    Main.py changes

--- main.py
+++ main.py
@@ -3,68 +3,29 @@ from GPT3 import *
 from streamlit_chat import message
 import time
 
-st.title("GPT-3 NLP")
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.title("Welcome To GPT-3 Learning Showcase👋")
+
+st.markdown(
+    """
+    ## What is GPT-3?
+    GPT-3 is a neural network machine learning model 
+    trained using internet data to generate any type of text. 
+    Developed by OpenAI, it requires a small amount of input text to generate large volumes of relevant 
+    and sophisticated machine-generated text.
+
+    ## What will you showcase?
+    - Chatbot
+    - Code Completion
+    - Images Generator
+    - Natural Language Process
+
+"""
+)
 
 
-tab1,tab2,tab3 = st.tabs(["Chatbot","Summarizer","Generate Text"])
-with tab1:
-    st.header("GPT-3 Chatbot with Characteristics")
-    def text():
-        input_user = st.text_input("USER:",key="input")
-        
-        
-        return input_user
-
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = []
-
-    if 'past' not in st.session_state:
-        st.session_state['past'] = []
-
-    characteristics = st.text_input("Characteristics",key="char")
-    
-    if characteristics:
-        user_input = text()
-        convo = list()
-        convo.append('USER: %s' % user_input)
-        text_block = '\n'.join(convo)
-        char = characteristics + text_block+"\nJAX: "
-        res = chatbot(char)
-
-    
-        if user_input:
-            output = chatbot(user_input)
-
-            st.session_state.past.append(user_input)
-            st.session_state.generated.append(res)
-
-        if st.session_state['generated']:
-
-            for i in range(len(st.session_state['generated'])-1, -1, -1):
-                message(st.session_state["generated"][i], key=str(i))
-                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-with tab2:
-    st.header("Summarization")
-
-    input_summary = st.text_area("Put your text")
-
-    summary = "Summarize this text :"
-    sum = list()
-    sum.append(input_summary)
-    if input_summary: 
-        test = summary + input_summary
-        res = proccess(test)
-        st.write(res)
-    else:
-        st.write("Please enter a text")   
-with tab3 :
-    st.header("Generate the text")
-    
-    input_gpt3 = st.text_input("What do you want to make?")
-    
-    if input_gpt3:
-        res = proccess(input_gpt3)
-        st.write(res)
-    else:
-        st.write("Please enter a text")
 
