a20d5e81a539a23100210e198c2d5b85653267db
AlekHesa <91420580+AlekHesa@users.noreply.github.com>
2023-03-14 15:04:48 +0700

    Update on API

--- API_gpt.py
+++ API_gpt.py
@@ -8,10 +8,18 @@ app = FastAPI()
 class TEXT(BaseModel):
     text : str
 
+class TOKEN(BaseModel):
+    key : str
+
+
 @app.get("/",tags=['Index'])
 def Index():
     return {"Content":"Hello World"}
 
+@app.post("/GPT3",tags=['GPT-3'])
+async def key(API_KEY : TOKEN):
+    api_key=apikey(API_KEY.key)
+    return api_key
 
 @app.post("/GPT3/Summary",tags=['GPT-3'])
 async def SUMMARY(text:TEXT):
@@ -33,7 +41,7 @@ async def AGBOT(text:TEXT):
     ]
     prompt = """
     You are an AI named AG-BOT that can help user to solve problems and also user can ask you a question about a certain words to know its meaning
-    and description. The problem that they can ask is related to Astragraphia needs such as digital printing, information technology, and any
+    and description. The problem that they can ask is related to technologies needs such as digital printing, information technology, and any
     technology related topic. if user ask anything that is not related to the topic mentioned before, do not answer the question and say 'please do not ask anything other than the topic i mention'
     """
 
