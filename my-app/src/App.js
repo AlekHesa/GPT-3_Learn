fdce9937172f5773371d679c89d122b57c9660b7
AlekHesa <91420580+AlekHesa@users.noreply.github.com>
2023-03-24 11:15:31 +0700

    Update on Branch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,65 +1,117 @@
 
 import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
-import './App.css';
-import ThemeSwitcher from './summary'
 import axios from 'axios';
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Navbar from "./components/navbar";
 
 
+function Summary_Pages() {
 
-function App() {
+  const[apikey, setAPIKey] = useState("");
 
   const[prompt,setPrompt] = useState("");
   const[response,setResponse] = useState("");
 
-  const prompt_api = {'text':prompt}
-
-  const json = JSON.stringify(prompt_api)
-
-  const handleSubmit = (e) =>{
+  const addApiKEY = (e) =>{
     e.preventDefault();
 
-    const data = { "data": prompt };
-    axios.post("http://127.0.0.1:8000/GPT3/Test", data)
+    // const data = { "data": prompt };
+    // axios.post("http://localhost:8000/GPT3/Test", 
+    // headers:{"Content-Type":'application/json'}
+    // ,data)
+    const data = {"key":apikey}
+    axios.post(
+      "http://localhost:8000/GPT3",
+      data,
+      {headers: {
+        'Content-Type':'application/json'
+      }}
+    )
     .then((res) =>{
-      setResponse(res.data)
+      
+      setAPIKey(res.data)
+       
+
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+  }
+
+  const handleChat = (e) =>{
+    e.preventDefault();
+
+    // const data = { "data": prompt };
+    // axios.post("http://localhost:8000/GPT3/Test", 
+    // headers:{"Content-Type":'application/json'}
+    // ,data)
+    const data = {"data":prompt}
+    axios.post(
+      "http://localhost:8000/GPT3/ag-bot",
+      data,
+      {headers: {
+        'Content-Type':'application/json'
+      }}
+    )
+    .then((res) =>{
+        setResponse(res.data)
     })
     .catch((err) => {
       console.error(err)
     })
   }
   return (
-    
-    <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
-    <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Text Summarizer</h1>
+  <div>
+    {/* <Navbar></Navbar> */}
+    <div className="container text-center">
+    <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">OpenAI Testing</h1>
     <h6 className="card text-white bg-info mb-3">React - OpenAI</h6>
-   <div className="card-body">
-    <form onSubmit={handleSubmit}>
-    <span className="card-text"> 
-      <textarea className="mb-2 form-control desIn"   
-      value={prompt}
-      onChange={(e) => setPrompt(e.target.value)}
-      placeholder='Summarize your text'/>
-    <button className="btn btn-outline-primary mx-2 mb-3" 
-    type="submit"
-    style={{'borderRadius':'50px',"font-weight":"bold"}}  >Summarize Text</button>
-    </span>
-    </form>
-    <h5 className="card text-white bg-dark mb-3">Your Tasks</h5>
-    <div >
-    <p>{response}</p>    
+    </div>
+    
+    {/*API KEY */}
+    <div className=" list-group-item text-center justify-content-center align-items-center mx-auto" style={{"width":"300px", "backgroundColor":"white", "marginTop":"15px"}} >
+      <form onSubmit={addApiKEY}>
+         
+          <input className="mb-2 form-control desIn"
+          type="password"  
+          value={apikey}
+          onChange={(e) => setAPIKey(e.target.value)}
+          placeholder='Summarize your text'/>
+          <button className="btn btn-outline-primary mx-2 mb-3" 
+          type="submit"
+          style={{'borderRadius':'50px',"font-weight":"bold"}}  >Add API Key</button>
+      </form>
+    </div>   
+
+<div className="list-group-item text-center justify-content-center align-items-center mx-auto" style={{"width":"500px", "backgroundColor":"white", "marginTop":"15px"}} >
+    <div className="card-body">
+      <h5 className="card text-white bg-primary mb-1">Text Summarizer</h5>
+        <form onSubmit={handleChat}>
+          <span className="card-text"> 
+            <textarea className="mb-2 form-control desIn"   
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder='Summarize your text'/>
+            <button className="btn btn-outline-primary mx-2 mb-3" 
+            type="submit"
+            style={{'borderRadius':'50px',"font-weight":"bold"}}  >Summarize Text</button>
+          </span>
+        </form>
+          <h5 className="card text-white bg-dark mb-3">Your Tasks</h5>
+          <div>
+          {response}
+          </div>
+      </div>
     </div>
     </div>
-  </div>
-     
-       
+  
  
     
    
   );
 }
 
-export default App;
+export default Summary_Pages;
--- my-app/src/FastAPI.js
+++ my-app/src/FastAPI.js
@@ -1,36 +1,88 @@
-import React, { useState } from 'react';
+import { useState } from "react";
+import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
 
-const {Configuration,OpenAIApi} = require("openai");
+
 
 function Website() {
-  const [text, setText] = useState('');
-  const [completion, setCompletion] = useState('');
-
-  const generateCompletion = async () => {
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
-      });
-      const openai = new OpenAIApi(configuration);
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: text,
-        max_tokens: 7,
-        temperature: 0,
-      });
-      setCompletion(response.choices[0].text)
-  };
-  const handleTextChange = (e) => {
-    setText(e.target.value);
-  };
-  return (
-    <div>
-      <form onSubmit={generateCompletion}>
-        <textarea placeholder="Enter text" value={text} onChange={handleTextChange} />
-        <button type="submit">Generate Summary</button>
-      </form>
-      {completion && <p>{completion}</p>}
+  const ThemeSwitcher = () => {
+    const [theme, setTheme] = useState(null);
+  
+    const resetTheme = () => {
+      setTheme(null);
+    };
+  
+  <div className="mb-2">
+      <Dropdown as={ButtonGroup} size="lg">
+        <Button
+          className="text-capitalize"
+          variant={theme ? theme : "secondary"}
+        >
+          {theme ? theme : "Default"}
+        </Button>
+        <Dropdown.Toggle
+          split
+          variant={theme ? theme : "secondary"}
+          id="dropdown-split-basic"
+        />
+        <Dropdown.Menu>
+          <Dropdown.Item eventKey="1" onClick={() => setTheme("primary")}>
+            Primary
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="2" onClick={() => setTheme("danger")}>
+            Danger
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="3" onClick={() => setTheme("success")}>
+            Success
+          </Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item eventKey="4" onClick={resetTheme}>
+            Default Theme
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
-  );
-}
+  
+  // // const [text, setText] = useState('');
+  // // const [completion, setCompletion] = useState('');
 
+  // // const generateCompletion = async () => {
+  // //   const configuration = new Configuration({
+  // //       apiKey: process.env.OPENAI_API_KEY,
+  // //     });
+  // //     const openai = new OpenAIApi(configuration);
+  // //     const response = await openai.createCompletion({
+  // //       model: "text-davinci-003",
+  // //       prompt: text,
+  // //       max_tokens: 7,
+  // //       temperature: 0,
+  // //     });
+  // //     setCompletion(response.choices[0].text)
+  // // };
+  // // const handleTextChange = (e) => {
+  // //   setText(e.target.value);
+  // // };
+  // // return (
+  // //   <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
+  // //   <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Text Summarizer</h1>
+  // //   <h6 className="card text-white bg-info mb-3">React - OpenAI</h6>
+  // //  <div className="card-body">
+  // //   <form >
+  // //   <span className="card-text"> 
+  // //     <textarea className="mb-2 form-control desIn"   
+  // //     placeholder='Summarize your text'/>
+  // //   <button className="btn btn-outline-primary mx-2 mb-3" 
+  // //   type="submit"
+  // //   style={{'borderRadius':'50px',"font-weight":"bold"}}  >Summarize Text</button>
+  // //   </span>
+  // //   </form>
+  // //   <h5 className="card text-white bg-dark mb-3">Your Tasks</h5>
+  // //   <div >
+  // //   <p></p>    
+  // //   </div>
+  // //   </div>
+  // // </div>
+  // );
+}
+}
 export default Website;
+
